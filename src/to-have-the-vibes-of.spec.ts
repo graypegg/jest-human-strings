dc7f39b3b6cd6c03e1b24b2a9a15c69438f4906c
Graham Pegg <graham.pegg@lightspeedhq.com>
2024-01-29 11:36:35 -0500

    One test green

--- src/to-have-the-vibes-of.spec.ts
+++ src/to-have-the-vibes-of.spec.ts
@@ -3,8 +3,31 @@ import { toHaveTheVibesOf } from "./to-have-the-vibes-of";
 expect.extend({ toHaveTheVibesOf });
 
 describe("toHaveTheVibesOf", () => {
-  it("should match strings with simple spaces and thin spaces", () => {
-    expect("my string").toHaveTheVibesOf("my string");
+  it("should match strings with simple spaces and thin spaces or no space at all", () => {
+    // Thin space
+    expect("my string").toHaveTheVibesOf("my string");
+    expect("my string").toHaveTheVibesOf("mystring");
+
+    // Hairline space
+    expect("my string").toHaveTheVibesOf("my string");
+    expect("my string").toHaveTheVibesOf("mystring");
+
+    // Punctuation space
+    expect("my string").toHaveTheVibesOf("my string");
+    expect("my string").toHaveTheVibesOf("mystring");
+  });
+
+  it("should match strings with simple spaces and wide spaces", () => {
+    expect("my string").toHaveTheVibesOf("my string");
+    //                ^ this is an EN space
+    expect("my string").toHaveTheVibesOf("my string");
+    //                ^ this is an EM space
+    expect("my string").toHaveTheVibesOf("my string");
+    //                ^ this is an EN Quad space
+    expect("my string").toHaveTheVibesOf("my string");
+    //                ^ this is an EM Quad space
+    expect("my string").toHaveTheVibesOf("my string");
+    //                ^ this is a figure space
   });
 
   it("should match strings with narrow non-breaking spaces to a regular space or no space at all, but not to a new line", () => {
--- src/to-have-the-vibes-of.ts
+++ src/to-have-the-vibes-of.ts
@@ -1,12 +1,44 @@
+import dedent from "dedent";
+import { diff } from "jest-diff";
+
+const TINY_SPACES = /[   ]/g;
+const REGULAR_SPACES = / /g;
+
+function strip<Values extends string | string[]>(
+  value: Values,
+  regex: RegExp | RegExp[],
+): Values {
+  if (Array.isArray(value)) {
+    return value.map((str) => strip(str, regex)) as Values;
+  }
+
+  if (!Array.isArray(regex)) return value.replace(regex, "") as Values;
+
+  if (regex.length === 0) return value;
+
+  return strip(strip(value, regex.at(-1)), regex.slice(0, -1));
+}
+
 export function toHaveTheVibesOf(
   received: string,
   expected: string,
 ): jest.CustomMatcherResult {
+  const [collapsedReceived, collapsedExpected] = strip(
+    [received, expected],
+    TINY_SPACES,
+  );
+
+  const doesLookTheSameWhenIgnoringSmallOrInvisibleSpaces =
+    collapsedReceived === collapsedExpected;
+
   return {
     message(): string {
-      return "alksjdflasjkdf";
+      return dedent`
+      Oh no
+      ${diff(expected, received)}
+      `;
     },
-    pass: false,
+    pass: doesLookTheSameWhenIgnoringSmallOrInvisibleSpaces,
   };
 }
 
