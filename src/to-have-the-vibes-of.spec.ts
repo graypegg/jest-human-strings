d81a873c327204d9f9b4d4d555fef31b5255b5bb
Graham Pegg <graham.pegg@lightspeedhq.com>
2024-01-29 10:43:11 -0500

    Some more tests

--- src/to-have-the-vibes-of.spec.ts
+++ src/to-have-the-vibes-of.spec.ts
@@ -4,10 +4,31 @@ expect.extend({ toHaveTheVibesOf });
 
 describe("toHaveTheVibesOf", () => {
   it("should match strings with simple spaces and thin spaces", () => {
-    expect("my string").toHaveTheVibesOf("my string");
+    expect("my string").toHaveTheVibesOf("my string");
   });
 
-  it("should match strings with and without non-breaking non-space characters", () => {
-    expect("mystring").toHaveTheVibesOf("my string");
+  it("should match strings with narrow non-breaking spaces to a regular space or no space at all, but not to a new line", () => {
+    expect("my string").toHaveTheVibesOf("my string");
+    expect("my string").toHaveTheVibesOf("mystring");
+    expect("my string").not.toHaveTheVibesOf("my\nstring");
+  });
+
+  it("should match strings with non-breaking spaces to a regular space, but not to a new line or no space at all", () => {
+    expect("my string").toHaveTheVibesOf("my string");
+    expect("my string").not.toHaveTheVibesOf("mystring");
+    expect("my string").not.toHaveTheVibesOf("my\nstring");
+  });
+
+  it("should match all horizontal lines", () => {
+    expect("my–string").toHaveTheVibesOf("my-string");
+    //                ^ this is an EN dash.
+    expect("my—string").toHaveTheVibesOf("my-string");
+    //                ^ this is an EM dash.
+    expect("my‒string").toHaveTheVibesOf("my-string");
+    //                ^ this is a figure dash.
+    expect("my‑string").toHaveTheVibesOf("my-string");
+    //                ^ this is a non-breaking hypen.
+    expect("my―string").toHaveTheVibesOf("my-string");
+    //                ^ this is a horizontal bar.
   });
 });
